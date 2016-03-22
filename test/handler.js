d679eff03cfc0b3fba059e814685841ea113f247
Austen Collins <austen@serverless.com>
2016-03-21 19:14:45 -0700

    Add babel runtime (WIP)

--- test/handler.js
+++
@@ -1,7 +0,0 @@
-export default (event, context) => {
-  // must return a promise, a JSON.stringify compatible data, null or nothing.
-  return {
-    message: 'Go Serverless! Your Lambda function executed successfully!'
-  }
-}
-
