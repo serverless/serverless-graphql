f647b6dcf8591d379d68b35ebe8823c0c9163dea
Brett Andrews <brett.j.andrews@gmail.com>
2016-03-26 18:49:08 -0700

    add console error when API_URL has not been set

--- client/src/app/js/actions/index.js
+++ client/src/app/js/actions/index.js
@@ -1,2 +1,6 @@
 // TODO: Replace with your API URL
-export const API_URL = 'https://example.com/stage';
+export const API_URL = '';
+
+if (!API_URL) {
+    console.error('Set `API_URL` in `index.js` to your stage endpoint')
+}
