cbfad28c23daac67362dccb6a478c0c8bc91100c
Egor Kislitsyn <egor@kislitsyn.com>
2016-03-29 21:20:32 +0700

    improves handler to return a promise instead of calling context.done()

--- back/api/data/handler.js
+++ back/api/data/handler.js
@@ -3,10 +3,7 @@
 var lib = require('../lib');
 
 module.exports.handler = function(event, context) {
-  lib.query(event.query)
-      .then(function (response) {
-        context.done(null, response);
-      });
+  return lib.query(event.query);
 };
 
 /*
