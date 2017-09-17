7d8d278c3503d888e2661484456dfc438c56df76
sid.gupta <sid.gupta@glassdoor.com>
2017-09-17 16:24:55 -0700

    remove webpack depdendencies

--- foundation/devServer/index.js
+++ foundation/devServer/index.js
@@ -1,31 +1,17 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import path from 'path';
-import favicon from 'serve-favicon';
-import webpack from 'webpack';
-import webpackDevMiddleware from 'webpack-dev-middleware';
-import webpackHotMiddleware from 'webpack-hot-middleware';
 import graphQLHandler from '../../graphql';
-import webpackConfig from '../webpack/webpack.dev';
 
-const APP_PORT = 4000;
-const app = express();
-const compiler = webpack(webpackConfig);
-const middleware = webpackDevMiddleware(compiler, {
-  noInfo: true,
-  publicPath: webpackConfig.output.publicPath,
-  silent: true,
-  stats: 'errors-only',
-});
+const SERVER_PORT = 4000;
+const server = express();
+server.use(bodyParser.json());
 
-app.use(bodyParser.json());
-
-app.use((req, res, next) => {
+server.use((req, res, next) => {
   console.log(new Date(), req.method, req.url);
   next();
 });
 
-app.post('/graphql', (req, res) => {
+server.post('/graphql', (req, res) => {
   graphQLHandler(req.body.query, req.body.variables)
     .then(result => {
       res.setHeader('Content-Type', 'application/json; charset=utf-8');
@@ -34,22 +20,7 @@ app.post('/graphql', (req, res) => {
     .catch(err => res.end(err));
 });
 
-app.use(middleware);
-app.use(webpackHotMiddleware(compiler));
-app.use(favicon(path.resolve(process.cwd(), 'app-client/public/favicon.ico')));
-
-app.get('*', (req, res) => {
-  const indexFilePath = path.join(compiler.outputPath, 'index.html');
-  middleware.fileSystem.readFile(indexFilePath, (err, file) => {
-    if (err) {
-      res.sendStatus(404);
-    } else {
-      res.send(file.toString());
-    }
-  });
-});
-
-app.listen(APP_PORT, (err) => {
+server.listen(SERVER_PORT, (err) => {
   if (err) console.log(err);
-  console.log(`App is now running on http://localhost:${APP_PORT}`);
+  console.log(`Server is now running on http://localhost:${SERVER_PORT}`);
 });
