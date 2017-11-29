874c29ac1b38e20afee82a93027b13ca082c0cec
sid.gupta <sid.gupta@glassdoor.com>
2017-11-28 22:46:02 -0800

    take first stab at AWSAppSyncClient

--- app-client/src/App.js
+++ app-client/src/App.js
@@ -3,32 +3,49 @@ import UserList from './components/UserList';
 import logo from './logo.svg';
 import './App.css';
 
-import { ApolloClient } from 'apollo-client';
+import AWSAppSyncClient from 'aws-appsync';
+import { Rehydrated } from 'aws-appsync-react';
+import { AUTH_TYPE } from 'aws-appsync/dist/link/auth-link';
 import { ApolloProvider } from 'react-apollo';
-import { HttpLink } from 'apollo-link-http';
-import { InMemoryCache } from 'apollo-cache-inmemory';
+import * as AWS from 'aws-sdk';
+import awsconfig from './aws-exports';
 
-const client = new ApolloClient({
-  link: new HttpLink({ uri: process.env.REACT_APP_GRAPHQL_ENDPOINT }),
-  cache: new InMemoryCache(),
+AWS.config.update({
+  region: awsconfig.REGION,
+  credentials: new AWS.Credentials({
+    accessKeyId: awsconfig.AWS_ACCESS_KEY_ID,
+    secretAccessKey: awsconfig.AWS_SECRET_ACCESS_KEY,
+  }),
+});
+
+const client = new AWSAppSyncClient({
+  url: process.env.REACT_APP_GRAPHQL_ENDPOINT,
+  region: 'us-east-1',
+  auth: { type: AUTH_TYPE.AWS_IAM, credentials: AWS.config.credentials },
 });
 
 class App extends Component {
   render() {
     return (
-      <ApolloProvider client={client}>
-        <div className="App">
-          <div className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <h2>Serverless GraphQL Apollo </h2>
-          </div>
-          <div className="App-User">
-            <UserList />
-          </div>
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Serverless GraphQL Apollo </h2>
         </div>
-      </ApolloProvider>
+        <div className="App-User">
+          <UserList />
+        </div>
+      </div>
     );
   }
 }
 
-export default App;
+const WithProvider = () => (
+  <ApolloProvider client={client}>
+    <Rehydrated>
+      <App />
+    </Rehydrated>
+  </ApolloProvider>
+);
+
+export default WithProvider;
