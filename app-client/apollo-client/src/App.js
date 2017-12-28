a9860509e8992d3fb6920d2973aa6aa68691a24e
sid.gupta <sid.gupta@glassdoor.com>
2017-12-28 12:01:57 +0530

    bring back apollo client integration

---
+++ app-client/apollo-client/src/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import UserList from './components/UserList';
+import logo from './logo.svg';
+import './App.css';
+
+import { ApolloClient } from 'apollo-client';
+import { ApolloProvider } from 'react-apollo';
+import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+
+const client = new ApolloClient({
+  link: new HttpLink({ uri: process.env.REACT_APP_GRAPHQL_ENDPOINT }),
+  cache: new InMemoryCache(),
+});
+
+class App extends Component {
+  render() {
+    return (
+      <ApolloProvider client={client}>
+        <div className="App">
+          <div className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h2>Serverless GraphQL Apollo </h2>
+          </div>
+          <div className="App-User">
+            <UserList />
+          </div>
+        </div>
+      </ApolloProvider>
+    );
+  }
+}
+
+export default App;
---
+++ app-client/appsync-client/src/components/UserList.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import { graphql } from 'react-apollo';
+import gql from 'graphql-tag';
+import PropTypes from 'prop-types';
+import User from './User';
+
+const UserList = ({ data: { loading, error, getTwitterFeed } }) => {
+  if (loading) {
+    return <p>Loading ...</p>;
+  }
+  if (error) {
+    return <p>{error.message}</p>;
+  }
+
+  return (
+    <div>
+      <User key={getTwitterFeed.name} user={getTwitterFeed} />
+    </div>
+  );
+};
+
+UserList.propTypes = {
+  data: PropTypes.any.isRequired, // eslint-disable-line
+};
+
+export const UserQuery = gql`
+  query UserQuery(
+    $handle: String!
+    $consumer_key: String!
+    $consumer_secret: String!
+  ) {
+    getTwitterFeed(
+      handle: $handle
+      consumer_key: $consumer_key
+      consumer_secret: $consumer_secret
+    ) {
+      name
+      location
+      screen_name
+      favourites_count
+      description
+      followers_count
+      friends_count
+      posts {
+        tweet
+      }
+    }
+  }
+`;
+
+export default graphql(UserQuery, {
+  options: () => ({
+    variables: {
+      handle: process.env.REACT_APP_HANDLE,
+      consumer_key: process.env.REACT_APP_CONSUMER_KEY,
+      consumer_secret: process.env.REACT_APP_SECRET_KEY,
+    },
+  }),
+})(UserList);
