33e0e4faddfc228e92cc9a6a67c4deb76c6e9337
sid.gupta <sid.gupta@glassdoor.com>
2017-12-30 00:33:52 +0530

    add persisted query support

--- app-client/apollo-client/src/App.js
+++ app-client/apollo-client/src/App.js
@@ -5,11 +5,17 @@ import './App.css';
 
 import { ApolloClient } from 'apollo-client';
 import { ApolloProvider } from 'react-apollo';
+import { createPersistedQueryLink } from 'apollo-link-persisted-queries';
 import { HttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 
+//enable persisted queries
+const apolloLink = createPersistedQueryLink().concat(
+  HttpLink({ uri: process.env.REACT_APP_GRAPHQL_ENDPOINT })
+);
+
 const client = new ApolloClient({
-  link: new HttpLink({ uri: process.env.REACT_APP_GRAPHQL_ENDPOINT }),
+  link: apolloLink,
   cache: new InMemoryCache(),
 });
 
