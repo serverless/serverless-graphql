5de1c796e1661811e232574aeca62512c398bf59
sid.gupta <sid.gupta@glassdoor.com>
2018-01-06 16:16:18 +0530

    Merge branch 'master' into feature/aws_xray_integration
    # Conflicts:
    #	app-backend/appsync/lambda/package.json

--- app-client/appsync-client/src/components/Tweetbar.js
+++ app-client/appsync-client/src/components/Tweetbar.js
@@ -1,34 +1,4 @@
 import React from 'react';
-import styled from 'styled-components';
-
-const Div = styled.div`
-  margin-top: 30px;
-  margin-bottom: 30px;
-`;
-
-const Container = styled.div`
-  margin: auto;
-  width: 600px;
-  max-width: 100%;
-  text-align: left;
-  background-color: #ffffff;
-  border-radius: 2px;
-  box-shadow: 0px 1px 1px 0 rgba(0, 0, 0, 0.3);
-`;
-
-const bar = styled.div`
-  margin-bottom: 30px;
-  padding: 15px 15px;
-  text-align: center;
-  & .bar {
-    color: #14171a;
-    font-size: 16px;
-    font-weight: bold;
-  }
-  & .material-icons {
-    font-size: 16px;
-  }
-`;
 
 export default class Tweetbar extends React.Component {
   constructor(props) {
@@ -45,16 +15,16 @@ export default class Tweetbar extends React.Component {
 
   render() {
     return (
-      <Div>
+      <div>
         <form onSubmit={this.handleSubmit}>
-          <Div>
-            <input type="text" size="50" value={this.state.value} />
-          </Div>
-          <Div>
+          <div>
+            <input type="text" size="50" placeholder={this.state.value} />
+          </div>
+          <div>
             <input type="submit" value="Tweet" />
-          </Div>
+          </div>
         </form>
-      </Div>
+      </div>
     );
   }
 }
