4ecde3b0c78d3bd038f89e858b31f30bb3d988dc
sid.gupta <sid.gupta@glassdoor.com>
2018-01-23 07:19:55 -0800

    add ES templates

--- app-backend/appsync/elasticsearch/seed-data/create-seed-data.js
+++ app-backend/appsync/elasticsearch/seed-data/create-seed-data.js
@@ -6,6 +6,8 @@ const uuidv4 = require('uuid/v4');
 
 let numRecords = 100;
 let numPosts = 25;
+const handleNames = [];
+const followersPerUser = 2;
 
 //{"index":{"_index":"user","_type":"profile","_id":"8144e3da-7521-4787-a95b-30f4081cd72e"}}
 
@@ -13,14 +15,26 @@ data = [];
 const file = 'users.json';
 
 for (let i = 0; i < numRecords; i++) {
+    const handle = faker.internet.userName();
+    handleNames.push(handle);
+}
+
+for (let i = 0; i < numRecords; i++) {
+  const followers = [];
+
+  for (let k = 0; k < followersPerUser; k++) {
+      followers.push(handleNames[Math.floor(Math.random() * handleNames.length)]);
+  }
+
   record = {
     name: faker.name.findName(),
-    screen_name: faker.internet.userName(),
+    handle: faker.internet.userName(),
     location: faker.address.city(),
     description: faker.name.jobTitle(),
     followers_count: faker.random.number(),
     friends_count: faker.random.number(),
     favourites_count: faker.random.number(),
+    followers: followers
   };
 
   for (let j = 0; j < numPosts; j++) {
@@ -35,6 +49,12 @@ for (let i = 0; i < numRecords; i++) {
     };
 
     record.tweet = faker.lorem.sentence();
+    record.retweeted = faker.random.boolean();
+    record.retweet_count = faker.random.number({
+      min: 1,
+      max: 50,
+    });
+    record.favorited = faker.random.boolean();
 
     console.log(JSON.stringify(item));
     console.log(JSON.stringify(record));
