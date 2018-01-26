bea2023f35eebd6bfb687dd2fe8d82adef5fef9f
sid.gupta <sid.gupta@glassdoor.com>
2018-01-25 18:42:48 -0800

    fix test

--- app-client/appsync-client/src/components/User.test.js
+++ app-client/appsync-client/src/components/User.test.js
@@ -6,17 +6,19 @@ it('render user name', () => {
   const user = {
     name: 'sid',
     location: 'SF',
-    screen_name: 'sidg_sid',
+    handle: 'sidg_sid',
     description: 'follow the universe',
     favourites_count: 200,
     followers_count: 300,
     friends_count: 100,
-    posts: [
-      {
-        name: 'sid',
-        tweet: 'this is my first tweet',
-      },
-    ],
+    tweets: {
+      items: [
+        {
+          tweet: 'this is my first tweet',
+          tweet_id: '123',
+        },
+      ],
+    },
   };
   const wrapper = shallow(<User user={user} />);
   expect(wrapper.html()).toContain(user.description);
