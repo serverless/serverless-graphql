19a9643f715d8145ed553ed0a1733e5742102350
Alek Merani <alekmerani@gmail.com>
2018-11-14 23:44:47 -0600

    Fix linter error

--- app-backend/dynamodb/resolvers.js
+++ app-backend/dynamodb/resolvers.js
@@ -4,8 +4,8 @@ import dynamodb from 'serverless-dynamodb-client';
 let docClient;
 
 if (process.env.NODE_ENV === 'production') {
-  const AWSXRay = require('aws-xray-sdk');
-  const AWS = AWSXRay.captureAWS(require('aws-sdk'));
+  const AWSXRay = require('aws-xray-sdk'); // eslint-disable-line global-require
+  const AWS = AWSXRay.captureAWS(require('aws-sdk')); // eslint-disable-line global-require
   docClient = new AWS.DynamoDB.DocumentClient();
 } else {
   docClient = dynamodb.doc;
